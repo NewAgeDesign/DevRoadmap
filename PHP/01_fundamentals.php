2b6a909595f4185069a3e09e3e555fc1a08774e3
newagedesign <tawinia6@gmail.com>
2025-08-29 08:18:10 +0300

    this works

--- PHP/01_fundamentals.php
+++ PHP/01_fundamentals.php
@@ -7,14 +7,14 @@ They are both the same, except for a few small difference:
 - Echo is marginaly faster than Print.
 */
 // Echo can be written as either one of the following, and yes, it also supports HTML:
-echo 'Hello World';
-echo('Hello World');
-echo 'different values '.'are joined together in echo '.'using the period symbol(.)';
+echo "Hello World \r\n";
+echo("Hello World \r\n");
+echo "different values "."are joined together in echo "."using the period symbol(.) \r\n";
 
 // The same works for print
-print 'Hello World';
-print('Hello World');
-print 'different values '.'are joined together in echo '.'using the period symbol(.)';
+print "Hello World \r\n";
+print("Hello World \r\n");
+print "different values "."are joined together in echo "."using the period symbol(.) \r\n";
 
 // When to use Print and Echo
 ?>
